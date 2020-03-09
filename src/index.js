0c50badfd76936d61bdd8d718bb49a0cb774b26e
BarbedWire <e.barbedwire@gmail.com>
2020-03-09 18:08:10 +0300

    feat: solve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  function checkBracket(token, idx) {
+    return bracketsConfig.map(pair => pair[idx]).filter(a => a === token).length !== 0;   
+  }
+  function isStartBracket(token) {
+    return checkBracket(token, 0)  
+  }
+
+  function isEndBracket(token) {
+    return checkBracket(token, 1)  
+  }
+
+  function getStartBracket(token) {
+    config = bracketsConfig.filter(pair => pair[1] === token);
+    if (config.length !== 0) {
+      return config[0][0];
+    } else {
+      return undefined;
+    }
+  }
+
+  const stack = [];
+  const result =  str.split('').reduce((a, s) => {
+    if (!a) return a;
+
+    if (isStartBracket(s)) {
+      if (isEndBracket(s) && stack[stack.length - 1] === s) {
+        stack.pop();
+        return true;
+      } else {
+        stack.push(s);
+        return true;
+      }
+    } else {
+      const b = stack.pop();
+      if (b) {
+        return getStartBracket(s) == b 
+      } else {
+        return false;
+      }
+    }
+  }, true);
+
+  return result && stack.length === 0;
 }
